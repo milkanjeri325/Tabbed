809c95e6cfd70bdf2849a88c118fbd8e9dbc05a6
milkanjeri <milkanjeri@gmail.com>
2022-08-01 10:54:23 +0300

    initial

---
+++ app/src/main/java/com/example/tabbed/GeeksFragment.kt
@@ -0,0 +1,17 @@
+package com.example.tabbed
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.ViewGroup
+
+class GeeksFragment : Fragment() {
+    // inflate the layout
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ) =
+        inflater.inflate(R.layout.fragment_geeks, container, false)!!
+}
---
+++ app/src/main/java/com/example/tabbed/ui/main/LeetFragment.kt
@@ -0,0 +1,19 @@
+package com.example.tabbed.ui.main
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.example.tabbed.R
+
+class LeetFragment : Fragment() {
+    // inflate the layout
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ) =
+        inflater.inflate(R.layout.fragment_leet, container, false)!!
+}
+
